c4cee928cf70ccc2506fa4fab37c7a424ab26cef
CarlosCRBS <carlos.crbs@gmail.com>
2022-07-27 00:33:23 +0000

    Aula 3

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from flask import Flask, render_template # Por  convenção o nome da classe começa com letra maiúscula
+from datetime import datetime
 
 app = Flask("hello")
 #app = Flask("index")
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+#mock do banco de dados -- simulação
+posts = [
+    {
+        "title": "O meu primeiro Post",
+        "body": "Aqui é o texto do 1º Post",
+        "author": "CarlosCRBS",
+        "created": datetime(2022,7,25)
+    },
+     {
+        "title": "O meu segundo Post",
+        "body": "Aqui é o texto do 2º Post",
+        "author": "CarlosCRBS",
+        "created": datetime(2022,7,26)
+    },
+]
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+
+
+
+#@app.route("/meucontato")
+#def meuContato():
+    #return render_template("index.html")
+ #   return render_template("index.html", nome="Carlos R B Santos", email="carlos.crbs@yahoo.com.br", telefone="5133409628")
     
 #    """
 #            <html>
